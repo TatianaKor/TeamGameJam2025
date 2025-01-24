5b435368897c4d0e3e1cadae35705d44651fd382
Alex <alexsmekhunov@gmail.com>
2025-01-24 23:00:49 +0300

    Added character movement. Added camera follow

--- Assets/Scenes/MainScene/PlayerController.cs
+++ Assets/Scenes/MainScene/PlayerController.cs
@@ -3,15 +3,30 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private InputActionMap _inputActionMap;
-    private InputAction move;
-    private InputAction throwGum;
+    [Header("Jump")]
+    [SerializeField] private float JUMP_ALLOWANCE_VERTICAL_DISTANCE = 1.7f;
+    [SerializeField] private float JUMP_ALLOWANCE_VERTICAL_VELOCITY = 0.1f;
+    [SerializeField] private float jumpPower = 4;
+    [SerializeField] private LayerMask foothold;
+
+    [Header("Movement")]
+    [SerializeField] private float movementAcceleration = 2;
     
+    [Header("Mandatory")]
+    [SerializeField] private Rigidbody2D rb;
+    
+    private InputActionMap _inputActionMap;
+    private InputAction _move;
+    private InputAction _jump;
+    private InputAction _throwGum;
+
     void Awake()
     {
         _inputActionMap = InputSystem.actions.FindActionMap("Player");
-        move = _inputActionMap.FindAction("Move");
-        throwGum = _inputActionMap.FindAction("ThrowGum");
+        _move = _inputActionMap.FindAction("Move");
+        _jump = _inputActionMap.FindAction("Jump");
+        _jump.performed += Jump;
+        _throwGum = _inputActionMap.FindAction("ThrowGum");
     }
 
     private void OnEnable()
@@ -27,6 +42,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var input = _move.ReadValue<float>();
+        // rb.AddForceX(input * movementAcceleration * Time.deltaTime);
+        rb.linearVelocityX = input * movementAcceleration;
+    }
+
+    private bool IsTouchingGround()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, JUMP_ALLOWANCE_VERTICAL_DISTANCE, foothold);
+        // Debug.DrawRay(transform.position, Vector2.down * JUMP_ALLOWANCE_VERTICAL_DISTANCE, Color.red, 10);
+        return hit.collider != null;
+    }
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        if (Mathf.Abs(rb.linearVelocityY) < JUMP_ALLOWANCE_VERTICAL_VELOCITY && IsTouchingGround())
+        {
+            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
     }
 }
